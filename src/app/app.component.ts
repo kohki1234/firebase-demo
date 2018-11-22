8797b961cefe9e74ecdbe88ee0ec5e27f0c49e95
Kohki Sato <you@example.com>
2018-11-22 19:33:06 +0900

    Added all file

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  courses$;
+  courses;
+  list$: AngularFireList<any[]>;
+
+  constructor(private db: AngularFireDatabase) {}
+
+  ngOnInit(): void {
+    this.courses = this.db.list('/courses');
+    this.list$ = this.courses.valueChanges();
+    this.courses$ = this.db.list('/courses').snapshotChanges().pipe(
+      map(changes => changes.map(change => ({
+        key: change.payload.key,
+        value: change.payload.val()
+      })))
+    );
+  }
+
+  add(course: HTMLInputElement) {
+    this.courses.push(course.value);
+    course.value = '';
+    }
+
+  update(course) {
+    this.db.object('/courses/' + course.key).set(course.value + ' UPDATED');
+  }
+
+  delete(course) {
+    this.db.object('/courses/' + course.key)
+      .remove()
+      .then(x => console.log('DELETED'));
+  }
+}
